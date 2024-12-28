ff1d9432bc0c0975757c10d0e78c392c36f7661e
Kirill Belikov <kirill-belikov@protonmail.com>
2024-12-29 12:20:55 +1300

    minimal setup

---
+++ keyboards/young_man_diy_sofle/config.h
@@ -0,0 +1,4 @@
+#pragma once
+
+/* Ensure we jump to bootloader if the QK_BOOT keycode was pressed */
+#define EARLY_INIT_PERFORM_BOOTLOADER_JUMP TRUE
---
+++ keyboards/young_man_diy_sofle/keymaps/default/keymap.c
@@ -0,0 +1,11 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       QK_BOOT,
+        KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,
+        KC_ESC,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,
+        KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,
+        KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LCTL,    KC_SPC,     KC_MUTE
+    )
+};
