2524a8bae5d6084d4c9777ac843545995d394ea1
Kirill Belikov <kirill-belikov@protonmail.com>
2025-01-05 15:35:59 +1300

    wip on split

--- keyboards/young_man_diy_sofle/config.h
+++ keyboards/young_man_diy_sofle/config.h
@@ -1,4 +1,8 @@
 #pragma once
 
-/* Ensure we jump to bootloader if the QK_BOOT keycode was pressed */
 #define EARLY_INIT_PERFORM_BOOTLOADER_JUMP TRUE
+
+#define SERIAL_USART_TX_PIN A2
+#define SERIAL_USART_TX_PAL_MODE 1
+
+#define SERIAL_USART_DRIVER SD2
--- keyboards/young_man_diy_sofle/keymaps/default/keymap.c
+++ keyboards/young_man_diy_sofle/keymaps/default/keymap.c
@@ -2,10 +2,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       QK_BOOT,
+        KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,
         KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,
         KC_ESC,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,
         KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,
-        KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LCTL,    KC_SPC,     KC_MUTE
+        KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LCTL,    KC_SPC,     QK_BOOT
     )
 };
